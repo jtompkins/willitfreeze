da09a036d6c3f0e0ba2c3de3854d3a0e88e69fea
Joshua Tompkins <josh@joshtompkins.com>
2015-01-13 16:46:30 -0500

    Bind data to the UI.

--- app.js
+++ app.js
@@ -1,29 +1,50 @@
 import $ from 'jquery';
+import ko from 'knockout';
+
 import LocationService from 'lib/locationService';
+import ForecastService from 'lib/forecastService';
 
 const FORECAST_KEY = '0f9abf3de31cd1840ae2678512ec1b07';
 const BING_KEY = 'Ap_46PGiEwOhWsR44SoFoYUO9_XaMRgXf7TdLHcU_6hWa3Br6hiV3fESATe9GtAh';
 
+class AppViewModel {
+	constructor() {
+		this.isBusy = ko.observable(false);
+
+		this.todaysLow = ko.observable(0.0);
+		this.tomorrowsLow = ko.observable(0.0);
+
+		this.city = ko.observable('');
+
+		this.load();
+	}
+
+	load() {
+		let locationService = new LocationService(BING_KEY);
+		let forecastService = new ForecastService(FORECAST_KEY);
+
+		locationService.getGeolocation()
+					.then((location) => {
+				 		var forecastTask = forecastService.getForecast(location);
+				 		var cityTask = locationService.getAddress(location);
+
+				 		return Promise.all([forecastTask, cityTask]);
+				 	})
+				 	.then((data) => {
+				 		let forecast = data[0];
+				 		let address = data[1];
+
+				 		this.city(address.locality);
+
+				 		let today = forecast.daily.data[0];
+				 		let tomorrow = forecast.daily.data[1];
+
+				 		this.todaysLow(today.temperatureMin);
+				 		this.tomorrowsLow(tomorrow.temperatureMin);
+				 	});
+	}
+}
+
 $(() => {
-	let service = new LocationService(FORECAST_KEY, BING_KEY);
-
-	service.getLocation()
-				.then((location) => {
-			 		var forecastTask = service.getForecast(location);
-			 		var cityTask = service.getCity(location);
-
-			 		return Promise.all([forecastTask, cityTask]);
-			 	})
-			 	.then((data) => {
-			 		let forecast = data[0];
-			 		let city = data[1];
-
-			 		console.log(`Location: ${city}`);
-
-			  	let today = forecast.daily.data[0];
-			 		let tomorrow = forecast.daily.data[1];
-
-			 		console.log(`Today's low: ${today.temperatureMin}`);
-			 		console.log(`Tomorrow's low: ${tomorrow.temperatureMin}`);
-			 	});
+	ko.applyBindings(new AppViewModel());
 });
--- config.js
+++ config.js
@@ -12,6 +12,7 @@ System.config({
     "format": "npm:format@0.2.1",
     "jquery": "github:components/jquery@2.1.3",
     "jsonp": "npm:jsonp@0.1.0",
+    "knockout": "npm:knockout@3.2.0",
     "github:jspm/nodelibs-fs@0.1.0": {
       "assert": "npm:assert@1.3.0",
       "fs": "github:jspm/nodelibs-fs@0.1.0"
@@ -44,6 +45,9 @@ System.config({
     },
     "npm:jsonp@0.1.0": {
       "debug": "npm:debug@2.1.1"
+    },
+    "npm:knockout@3.2.0": {
+      "process": "github:jspm/nodelibs-process@0.1.0"
     },
     "npm:util@0.10.3": {
       "inherits": "npm:inherits@2.0.1",
