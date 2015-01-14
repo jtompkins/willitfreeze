0c9f7acd546ecdb2a403d8a620ed7971467e1e30
Joshua Tompkins <josh@joshtompkins.com>
2015-01-14 14:47:55 -0500

    Pull images from Flickr for location.

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import * as Constants from 'lib/constants';
 import LocationService from 'lib/locationService';
 import ForecastService from 'lib/forecastService';
 import StorageService from 'lib/storageService';
+import PhotoService from 'lib/photoService';
 
 class AppViewModel {
 	constructor() {
@@ -15,8 +16,23 @@ class AppViewModel {
 		this.tomorrowsLow = ko.observable(0.0);
 
 		this.city = ko.observable('');
+		this.photo = ko.observable('');
+
+		this.photoCss = ko.computed(() => {
+		    return `url(${this.photo()})`;
+		});
 
 		this.load();
+	}
+
+	getRandomInt(min, max) {
+	  return Math.floor(Math.random() * (max - min)) + min;
+	}
+
+	pickRandomPhoto(photos) {
+		let index = this.getRandomInt(0, photos.length);
+
+		return photos[index];
 	}
 
 	populate(address, forecast) {
@@ -43,17 +59,30 @@ class AppViewModel {
 
 		let locationService = new LocationService(Constants.BING_KEY);
 		let forecastService = new ForecastService(Constants.FORECAST_KEY);
+		let photoService = new PhotoService(Constants.FLICKR_KEY);
 
 		locationService.getGeolocation()
 					.then((location) => {
 				 		let forecastTask = forecastService.getForecast(location);
 				 		let addressTask = locationService.getAddress(location);
+				 		let photoTask = photoService.getPhotos(location);
 
-				 		return Promise.all([addressTask, forecastTask]);
+				 		return Promise.all([addressTask, forecastTask, photoTask]);
 				 	})
 				 	.then((data) => {
 				 		let address = data[0];
 				 		let forecast = data[1];
+				 		let photos = data[2];
+
+				 		if (photos) {
+					 		let photo = this.pickRandomPhoto(photos);
+
+					 		if(photo) {
+						 		let photoUrl = photoService.getPhotoUrl(photo);
+
+						 		this.photo(photoUrl);
+						 	}
+				 		}
 
 				 		this.populate(address, forecast);
 				 	});
