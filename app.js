29836b609b6dc54131763fa6d66e877a3046e1f9
Joshua Tompkins <josh@joshtompkins.com>
2015-01-14 08:13:21 -0500

    Move storage logic to view model.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import * as Constants from 'lib/constants';
 
 import LocationService from 'lib/locationService';
 import ForecastService from 'lib/forecastService';
+import StorageService from 'lib/storageService';
 
 class AppViewModel {
 	constructor() {
@@ -18,7 +19,28 @@ class AppViewModel {
 		this.load();
 	}
 
+	populate(address, forecast) {
+		this.city(address.locality);
+
+ 		let today = forecast.daily.data[0];
+ 		let tomorrow = forecast.daily.data[1];
+
+ 		this.todaysLow(today.temperatureMin);
+ 		this.tomorrowsLow(tomorrow.temperatureMin);
+	}
+
 	load() {
+		let storageService = new StorageService(Constants.LOCAL_STORAGE_EXPIRATION);
+
+		let savedAddress = storageService.get(Constants.SAVED_ADDRESS_KEY);
+		let savedForecast = storageService.get(Constants.SAVED_FORECAST_KEY);
+
+		if (savedAddress && savedForecast) {
+			this.populate(savedAddress, savedForecast);
+
+			return;
+		}
+
 		let locationService = new LocationService(Constants.BING_KEY);
 		let forecastService = new ForecastService(Constants.FORECAST_KEY);
 
@@ -33,13 +55,7 @@ class AppViewModel {
 				 		let forecast = data[0];
 				 		let address = data[1];
 
-				 		this.city(address.locality);
-
-				 		let today = forecast.daily.data[0];
-				 		let tomorrow = forecast.daily.data[1];
-
-				 		this.todaysLow(today.temperatureMin);
-				 		this.tomorrowsLow(tomorrow.temperatureMin);
+				 		this.populate(address, forecast);
 				 	});
 	}
 }
